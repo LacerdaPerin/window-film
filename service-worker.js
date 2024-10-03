a2b119f784356f0b57f248099bbb1abd6be00800
Window Tinting App <alexlacerdaperin@gmail.com>
2024-10-03 11:41:35 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,52 @@
+const CACHE_NAME = 'app-cache-v25'; // Nome do cache, altere a cada nova versão
+
+const urlsToCache = [
+    '/peliqueiros-br/',
+    '/peliqueiros-br/index.html',
+    '/peliqueiros-br/icons/menor.png',
+    '/peliqueiros-br/icons/maior.png',
+    '/peliqueiros-br/manifest.json',
+    // Adicione outros recursos necessários para o funcionamento offline aqui
+];
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('app-cache').then((cache) => {
-      return cache.addAll([
-        '/teste-pwa/',
-        '/teste-pwa/index.html',
-        '/teste-pwa/icons/menor.png',
-        '/teste-pwa/icons/maior.png',
-        // Adicione outros recursos necessários para o funcionamento offline aqui
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Arquivos em cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting(); // Força o Service Worker a ser ativado imediatamente
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Removendo cache antigo:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim(); // Força todas as abas a usar o novo Service Worker
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Serve o arquivo em cache, se disponível; caso contrário, faz uma requisição
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
 });
